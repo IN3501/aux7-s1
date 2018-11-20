05bfb890b95a7495ca176fb5a503d4c275823dce
mi nombre <miemail@gmail.com>
2018-11-20 17:00:04 -0300

    metodo str usuarios

--- base_auxs/app_de_IN3501/models.py
+++ base_auxs/app_de_IN3501/models.py
@@ -188,6 +188,9 @@ class Usuarios(models.Model):
     password = models.CharField(max_length=64)
     email = models.CharField(max_length=50)
 
+    def __str__(self):
+    	return self.nombre + " " + self.apellido
+
     class Meta:
         managed = False
         db_table = 'usuarios'
